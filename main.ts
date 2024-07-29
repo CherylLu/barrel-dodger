a586d76b20d6c5673c1ec30a1390b2ec35f0f66a
CherylLu <35189430+CherylLu@users.noreply.github.com>
2024-07-29 13:53:32 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,51 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vy = -200
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    game.over(false)
+})
+let projectile: Sprite = null
+let mySprite: Sprite = null
+tiles.setTilemap(tilemap`層級7`)
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f e e 2 2 2 2 2 2 e f f . . 
+    . f f e 2 f f f f f f 2 e f f . 
+    . f f f f f e e e e f f f f f . 
+    . . f e f b f 4 4 f b f e f . . 
+    . . f e 4 1 f d d f 1 4 e f . . 
+    . . e f e 4 d d d d 4 e f . . . 
+    . . e 4 d d e 2 2 2 2 f e f . . 
+    . . . e d d e 2 2 2 2 f 4 e . . 
+    . . . . e e f 5 5 4 4 f . . . . 
+    . . . . . f f f f f f f . . . . 
+    . . . . . . . . . f f f . . . . 
+    `, SpriteKind.Player)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 6))
+mySprite.ay = 500
+game.onUpdateInterval(2000, function () {
+    projectile = sprites.createProjectileFromSide(img`
+        . . f f f . . . . . . . . f f f 
+        . f f c c . . . . . . f c b b c 
+        f f c c . . . . . . f c b b c . 
+        f c f c . . . . . . f b c c c . 
+        f f f c c . c c . f c b b c c . 
+        f f c 3 c c 3 c c f b c b b c . 
+        f f b 3 b c 3 b c f b c c b c . 
+        . c b b b b b b c b b c c c . . 
+        . c 1 b b b 1 b b c c c c . . . 
+        c b b b b b b b b b c c . . . . 
+        c b c b b b c b b b b f . . . . 
+        f b 1 f f f 1 b b b b f c . . . 
+        f b b b b b b b b b b f c c . . 
+        . f b b b b b b b b c f . . . . 
+        . . f b b b b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        `, Math.randomRange(-100, -80), 0)
+    tiles.placeOnTile(projectile, tiles.getTileLocation(9, 6))
+    info.changeScoreBy(1)
+})
